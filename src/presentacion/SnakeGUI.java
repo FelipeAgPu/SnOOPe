fb2ddfc5e66f3f2c6e7ef2907409cb6d7fa838fa
Juan Felipe Aguas Pulido <aguasfelipe02@gmail.com>
2021-04-26 20:14:00 -0500

    Added class diagrams and created project structure

---
+++ src/presentacion/SnakeGUI.java
@@ -0,0 +1,27 @@
+package presentacion;
+
+import javax.swing.*;
+
+public class SnakeGUI extends JFrame {
+
+    public SnakeGUI(){
+        prepareElementos();
+        prepareAcciones();
+    }
+
+    private void prepareElementos() {
+        this.setTitle("SnOOPe");
+        //Creación Ventana Ppal
+
+    }
+
+    private void prepareAcciones() {
+
+    }
+
+
+
+    public static void main(String[] args) {
+        SnakeGUI gui = new SnakeGUI();
+    }
+}
